35f2e741afb53d0976abe33d4099668fcafb4284
gpraveencr <tenpowerofnine@gmail.com>
2022-11-16 10:28:05 -0600

    added DynamoDB terragorm script

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_dynamodb_table" "product_table" {
+  name         = "PRODUCT"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "product_id"
+
+  attribute {
+    name = "product_id"
+    type = "S"
+  }
+  attribute {
+    name = "category"
+    type = "S"
+  }
+  attribute {
+    name = "product_rating"
+    type = "N"
+  }
+
+  global_secondary_index {
+    name            = "ProductCategoryRatingIndex"
+    hash_key        = "category"
+    range_key       = "product_rating"
+    projection_type = "ALL"
+  }
+}
+
